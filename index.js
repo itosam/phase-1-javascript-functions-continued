6e9644fe2eee3bd1cd2ba1aa2107d1827bca8265
Osamu Ito <mr.samito@gmail.com>
2022-09-02 15:04:25 -0400

    commit tree c638ba8ba003288f073c656b9ca71b9b0eff7c76

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// function declaration
+// function razzle () { 
+//     console.log("You've been razzled!") }
+
+// razzle();
+
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+
+const mondayWork = function(monday = "go to the office") {
+    return `This Monday, I will ${monday}.`
+}
+
+let wrapAdjective = function(highlight = "*") {
+    return function(adj = "special") {
+        return `You are ${highlight}${adj}${highlight}!`
+};
+}
